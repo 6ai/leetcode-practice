4bbc41ba65ac25d08f7e1600674850c8f71d9e6b
nattle <eliseyzubkov@gmail.com>
2020-05-12 00:48:25 +0300

    3. Longest Substring Without Repeating Characters

---
+++ 3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,36 @@
+# 3. Longest Substring Without Repeating Characters
+
+class Solution:
+    def isPalidrome(self, s) -> bool:
+        return s[::-1] == s
+
+    def longestPalidromeAtIndex(self, index: int, s: str) -> str:
+        longestPalidrome = s[index]
+
+        if index == 0:
+            return longestPalidrome
+
+        shift = 1
+        currentSubstring = s[index - shift: index + shift + 1]
+        while self.isPalidrome(currentSubstring) and shift <= index and index + shift < len(s):
+            longestPalidrome = currentSubstring
+            shift += 1
+            currentSubstring = s[index - shift: index + shift + 1]
+        
+        shift = 1
+        currentSubstring = s[index - shift: index + shift]
+        while self.isPalidrome(currentSubstring) and shift <= index and index + shift - 1 < len(s):
+            if len(currentSubstring) > len(longestPalidrome):
+                longestPalidrome = currentSubstring
+            shift += 1
+            currentSubstring = s[index - shift: index + shift]
+
+        return longestPalidrome
+    
+    def longestPalindrome(self, s: str) -> str:
+        longestPalidrome = ''
+        for index in range(len(s)):
+            currentPalidrome = self.longestPalidromeAtIndex(index, s)
+            if len(currentPalidrome) > len(longestPalidrome):
+                longestPalidrome = currentPalidrome 
+        return longestPalidrome
