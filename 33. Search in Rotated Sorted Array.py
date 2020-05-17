2efcf1578eb0c9cce977034684ae28d891694527
nattle <eliseyzubkov@gmail.com>
2020-05-17 20:20:39 +0300

    33. Search in Rotated Sorted Array

---
+++ 33. Search in Rotated Sorted Array.py
@@ -0,0 +1,8 @@
+# 33. Search in Rotated Sorted Array
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
